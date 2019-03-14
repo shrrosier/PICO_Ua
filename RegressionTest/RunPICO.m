78c17edeb48c7c012751748623760f7650a5b315
tkqh5 <tkqh5@C17777348.campus.unn.ac.uk>
2019-03-14 10:10:33 +0000

    minor changes

--- RegressionTest/RunPICO.m
+++ RegressionTest/RunPICO.m
@@ -4,7 +4,7 @@
 
 
 %% Load data
-clearvars
+
 load ResultsFiles/0000001-RegressionTest.mat;
 %%
 addpath(genpath('../')); % PATH TO PICO
@@ -19,17 +19,15 @@ y = MUA.coordinates(:,2);
 %%
 % Run PICO:
 PICO_opts = struct;
-PICO_opts.minArea = 1000;
 PICO_opts.algorithm = 'polygon';
 PICO_opts.C1 = 1e6;
 PICO_opts.gamTstar = 2e-5;
 PICO_opts.nmax = 5;
 PICO_opts.SmallShelfMelt = 0;
-PICO_opts.PICOres = 500;
-PICO_opts.Tbasins = -1.0;
-PICO_opts.Sbasins = 34.5; 
-load('MeshBoundaryCoordinates.mat');
-PICO_opts.MeshBoundaryCoordinates = MeshBoundaryCoordinates;
+PICO_opts.PICOres = 6000;
+PICO_opts.Tbasins = [-1.76;-1.66;-1.65;-1.58;-1.51;-1.73;-1.68;-0.73;-1.61;-1.30;-1.83;-1.58;-0.36;0.47;1.04;1.17;0.23;-1.23;-1.80];
+PICO_opts.Sbasins = [34.82;34.70;34.48;34.49;34.5;34.70;34.65;34.73;34.75;34.84;34.95;34.79;34.58;34.73;34.86;34.84;34.70;34.76;34.84];
+PICO_opts.MeshBoundaryCoordinates = 'MeshBoundaryCoordinates.mat'
 
 tic
 
@@ -53,12 +51,12 @@ Mk_log = sign(Mk).*log10(abs(Mk)*10^decimals);
 
 %
 figure; hold all;
-PlotMeshScalarVariable(CtrlVar, MUA, Mk_log)
+PlotMeshScalarVariable(CtrlVarInRestartFile, MUA, Mk_log)
 cbar = colorbar; caxis([-log10(0.1*10^decimals) log10(30*10^decimals)]);
 cbar.Label.String = 'sub-shelf melting (m/a)';
 cbar.TickLabels = {'-0.1' ,'0', '0.1', '1', '5', '10', '30'};
 cbar.Ticks = [-log10(0.1*10^decimals) 0  log10(0.1*10^decimals)  log10(1*10^decimals) log10(5*10^decimals) log10(10*10^decimals) log10(30*10^decimals)];
-PlotGroundingLines(CtrlVar, MUA, GF); PlotBoundary(MUA.Boundary,MUA.connectivity,MUA.coordinates,CtrlVar);
+PlotGroundingLines(CtrlVarInRestartFile, MUA, GF); PlotBoundary(MUA.Boundary,MUA.connectivity,MUA.coordinates,CtrlVarInRestartFile);
 
 %% aggregate melt rates over the ice shelves (first guess, ignore the area of the elements)
 
