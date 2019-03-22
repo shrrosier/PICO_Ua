b3d30c2b2fc1d9081dc121505e68703479dba471
Ronja Reese <reese@pik-potsdam.de>
2019-03-22 12:15:18 +0100

    Update PICO inputs for Antarctic example

--- RunPICO.m
+++ RunPICO.m
@@ -14,7 +14,7 @@ y = MUA.coordinates(:,2);
 %%
 % Run PICO:
 PICO_opts = struct;
-PICO_opts.algorithm = 'watershed';%'polygon'; %'watershed';
+PICO_opts.algorithm = 'polygon';%'polygon';
 PICO_opts.C1 = 1e6;
 PICO_opts.gamTstar = 2e-5;
 PICO_opts.nmax = 5;
@@ -23,14 +23,14 @@ PICO_opts.PICOres = 6000;
 PICO_opts.BasinsFile = 'BasinsInterpolant.mat';
 PICO_opts.FloatingCriteria = 'GLthreshold'; %'GLthreshold' or 'StrictDownstream'
 
-PICO_opts.Sbasins = [34.6505; 34.5273; 34.3222; 34.3259; 34.3297; 34.5315; 34.4819; 34.5666; 34.5766; 34.6677; 34.7822; 34.6254; 34.4107; 34.5526; 34.6902; 34.6668; 34.5339; 34.5849; 34.6644];
-PICO_opts.Tbasins = [-1.76;-1.66;-1.65;-1.58;-1.51;-1.73;-1.68;-0.73;-1.61;-1.30;-1.83;-1.58;-0.36;0.47;1.04;1.17;0.23;-1.23;-1.80];
-% PICO_opts.Sbasins = [34.82;34.70;34.48;34.49;34.5;34.70;34.65;34.73;34.75;34.84;34.95;34.79;34.58;34.73;34.86;34.84;34.70;34.76;34.84];
+PICO_opts.Sbasins = [34.6505;34.5273;34.3222;34.3259;34.3297;34.5315;34.4819;34.5666;34.5766;34.6677;34.7822;34.6254;34.4107;34.5526;34.6902;34.6668;34.5339;34.5849;34.6644];
+PICO_opts.Tbasins = [-1.75725;-1.65931;-1.58212;-1.54757;-1.51301;-1.72267;-1.69117;-0.67609;-1.61561;-1.30789;-1.83764;-1.5798;-0.368398;0.45505;1.04046;1.17196;0.229878;-1.23091;-1.79334];
+
 PICO_opts.MeshBoundaryCoordinates = MeshBoundaryCoordinates;
 
 tic
 
-[Mk,ShelfID,T0,S0,Tkm,Skm,q,PBOX] = PICO_driver(CtrlVarInRestartFile,MUA,GF,F.h,F.rho, PICO_opts);
+[Mk,ShelfID,T0,S0,Tkm,Skm,q,PBOX,Ak] = PICO_driver(CtrlVarInRestartFile,MUA,GF,F.h,median(F.rho),F.rhow,PICO_opts);
 
 toc
 
